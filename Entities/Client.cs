1962c51e17db53faa96bc2daa499e507e40a25ae
AnnaBea10 <pereiradelima938@gmail.com>
2023-12-26 13:39:38 -0300

    Entities created

---
+++ Entities/Client.cs
@@ -0,0 +1,30 @@
+﻿
+namespace spreadsheet.Entities
+{
+    public class Client
+    {
+        private int id { get; set; }
+        private String name { get; set; }
+        private String phone { get; set; }
+        private String email { get; set; }
+
+        public Client(int id, string name, string phone, string email)
+        {
+            this.id = id;
+            this.name = name;
+            this.phone = phone;
+            this.email = email;
+        }
+
+        //HashCode and Equals
+        public override bool Equals(object? obj)
+        {
+            return obj is Client client &&
+                   id == client.id;
+        }
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(id);
+        }
+    }
+}
---
+++ Entities/Enterprise.cs
@@ -0,0 +1,34 @@
+﻿namespace spreadsheet.Entities
+{
+    public class Enterprise
+    {
+        private int id { get; set;}
+        private String name { get; set;}
+        private String description { get; set; }
+        private String adress { get; set; }
+        private double price { get; set; } 
+
+        public Enterprise(int id, String name, String description, String adress, double price)
+        {
+            this.id = id;   
+            this.name = name;
+            this.description = description;
+            this.adress = adress;
+            this.price = price;
+        }
+
+       
+
+        //HashCode and Equals
+        public override bool Equals(object? obj)
+        {
+            return obj is Enterprise enterprise &&
+                   id == enterprise.id;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(id);
+        }
+    }
+}
